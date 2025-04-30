7daa9e441ffc45627ed78de4845e26e230e58e24
germani1978 <germanibenitezalfonso1@gmail.com>
2025-04-30 17:06:46 -0400

    1.7

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,18 +8,40 @@ function App() {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+
+    const handleGood = () => {
+        setGood(good + 1)
+        setAll(all + 1)
+    }
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+    }
+    const handleBad = () => {
+        setBad(bad + 1)
+        setAll(all + 1)
+    }
+
+    const average = () =>
+        all !== 0 ? (good * 1 + neutral * 0 + bad * -1) / all : 0
+
+    const positive = () => (all !== 0 ? (good / all) * 100 : 0)
 
     return (
         <div>
             <h1>give feedback</h1>
-            <Button text="good" handle={() => setGood(good + 1)} />
-            <Button text="neutral" handle={() => setNeutral(neutral + 1)} />
-            <Button text="bad" handle={() => setBad(bad + 1)} />
+            <Button text="good" handle={handleGood} />
+            <Button text="neutral" handle={handleNeutral} />
+            <Button text="bad" handle={handleBad} />
 
             <h2>Statistics</h2>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average()}</p>
+            <p>positive {positive()}%</p>
         </div>
     )
 }
