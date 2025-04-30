086d3a3c9dc5a9531d5772007f99bef3f0f9b9f1
germani1978 <germanibenitezalfonso1@gmail.com>
2025-04-30 17:12:30 -0400

    1.8

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,20 @@ const Button = ({ text, handle }) => {
     return <button onClick={handle}>{text}</button>
 }
 
+const Statistics = ({ good, neutral, bad, all, average, positive }) => {
+    return (
+        <>
+            <h2>Statistics</h2>
+            <p>good {good}</p>
+            <p>neutral {neutral}</p>
+            <p>bad {bad}</p>
+            <p>all {all}</p>
+            <p>average {average()}</p>
+            <p>positive {positive()}%</p>
+        </>
+    )
+}
+
 function App() {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
@@ -35,13 +49,14 @@ function App() {
             <Button text="neutral" handle={handleNeutral} />
             <Button text="bad" handle={handleBad} />
 
-            <h2>Statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average()}</p>
-            <p>positive {positive()}%</p>
+            <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                all={all}
+                average={average}
+                positive={positive}
+            />
         </div>
     )
 }
