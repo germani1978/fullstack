7bdbc56e226c1e760a1ade67d181979eb202ad07
germani1978 <germanibenitezalfonso1@gmail.com>
2025-04-30 17:22:30 -0400

    1.9

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,12 +8,18 @@ const Statistics = ({ good, neutral, bad, all, average, positive }) => {
     return (
         <>
             <h2>Statistics</h2>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {all}</p>
-            <p>average {average()}</p>
-            <p>positive {positive()}%</p>
+            {all !== 0 ? (
+                <>
+                    <p>good {good}</p>
+                    <p>neutral {neutral}</p>
+                    <p>bad {bad}</p>
+                    <p>all {all}</p>
+                    <p>average {average()}</p>
+                    <p>positive {positive()}%</p>
+                </>
+            ) : (
+                <p>No feedback given</p>
+            )}
         </>
     )
 }
