4e2a96849e0c4adb9ff3b7a42cab61a606605ac5
germani1978 <germanibenitezalfonso1@gmail.com>
2025-05-01 21:46:03 -0400

    2.1

---
+++ part2/2.1/src/components/Content.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Part from './Part'
+
+function Content({ parts }) {
+    return (
+        <div>
+            {parts.map(part => (
+                <Part
+                    key={part.id}
+                    name={part.name}
+                    exercises={part.exercises}
+                />
+            ))}
+        </div>
+    )
+}
+
+export default Content
