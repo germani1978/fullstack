48dadf67b65919d8555cf797de9cec111472937d
germani1978 <germanibenitezalfonso1@gmail.com>
2025-05-10 20:13:50 -0400

    2.20

--- part2/country/src/components/Board.jsx
+++ part2/country/src/components/Board.jsx
@@ -1,13 +1,42 @@
+import axios from 'axios'
+import { useEffect, useState } from 'react'
+
 const Board = ({ countries, setSearchInput, isLoading }) => {
+    const [weather, setWeather] = useState(null)
+
+    useEffect(() => {}, [weather])
+
+    useEffect(() => {
+        if (countries && countries.length == 1) {
+            console.log(countries[0].name.common)
+            axios
+                .get(
+                    `${import.meta.env.VITE_URL}q=${
+                        countries[0].name.common
+                    }&appid=${import.meta.env.VITE_APPID}&units=metric`
+                )
+                .then(response => setWeather(response.data))
+                .catch(err => {
+                    console.log(err.message)
+                    setWeather(null)
+                })
+        }
+    }, [countries])
+
+    //loading
     if (isLoading) return <div>Loading ...</div>
 
+    //
     if (countries == null) return <div>Start typing to search</div>
 
+    //not found countries
     if (countries.length == 0) return <div>No countries found</div>
 
+    //to many countries
     if (countries.length > 10)
         return <div>Too many matches, specify another filter</div>
 
+    //only one searched
     if (countries.length == 1) {
         const country = countries[0]
 
@@ -22,6 +51,18 @@ const Board = ({ countries, setSearchInput, isLoading }) => {
                 ))}
                 <p></p>
                 <img src={country.flags.png} alt="Flag" />
+                <p></p>
+                <h2>Weather in {countries[0].name.common}</h2>
+                {weather && (
+                    <div>
+                        <p>Temperature: {weather.main.temp} Celsius</p>
+                        <img
+                            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                            alt="img"
+                        />
+                        <p>Wind: {weather.wind.speed}</p>
+                    </div>
+                )}
             </div>
         )
     }
